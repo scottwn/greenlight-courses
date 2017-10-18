e5b54354d3b20807d4b45ed043a9614f262cb504
Scott Wyman Neagle <scott.neagle@cs.nyu.edu>
2017-10-18 15:27:55 -0600

    added procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java $JVM_OPTS -cp target/greenlight-standalone.jar clojure.main -m greenlight.web
--- project.clj
+++ project.clj
@@ -1,6 +1,16 @@
-(defproject greenlight "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject greenlight "0.0.1"
+  :description "Green Light web app"
+  :url "http://greenlight-courses.herokuapp.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [compojure "1.4.0"]
+                 [ring/ring-jetty-adapter "1.4.0"]
+                 [environ "1.0.0"]
+                 [org.clojure/java.jdbc "0.3.5"]
+                 [org.postgresql/postgresql "9.4-1201-jdbc4"]]
+  :min-lein-version "2.0.0"
+  :plugins [[environ/environ.lein "0.3.1"]]
+  :hooks [environ.leiningen.hooks]
+  :uberjar-name "greenlight-standalone.jar"
+  :profiles {:production {:env {:production true}}})
