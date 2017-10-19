e575b010610505d0e21ae8f37db0d36f1a6acd0c
Scott Wyman Neagle <scott.neagle@cs.nyu.edu>
2017-10-19 13:15:29 -0600

    begin building web interface

---
+++ src/greenlight/web.clj
@@ -0,0 +1,13 @@
+(ns greenlight.web
+  (:require [compojure.core :refer [defroutes GET PUT POST DELETE ANY]]
+            [compojure.handler :refer [site]]
+            [compojure.route :as route]
+            [clojure.java.io :as io]
+            [ring.adapter.jetty :as jetty]
+            [environ.core :refer [env]]
+            [clojure.java.jdbc :as db]))
+
+(defn splash []
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body (
