2ce928365067ddce9499e3f7aa73f88b931ad1f7
Scott Wyman Neagle <scott.neagle@cs.nyu.edu>
2017-10-19 16:23:37 -0600

    try form

--- src/greenlight/web.clj
+++ src/greenlight/web.clj
@@ -7,7 +7,42 @@
             [environ.core :refer [env]]
             [clojure.java.jdbc :as db]))
 
-(defn splash []
-  {:status 200
-   :headers {"Content-Type" "text/html"}
-   :body (
+;(defn splash []
+;  {:status 200
+;   :headers {"Content-Type" "text/html"}
+;   :body (
+
+(defn view-layout [& content]
+  (html
+    (doctype :xhtml-strict)
+    (xhtml-tag "en"
+      [:head
+        [:meta {:http-equiv "Content-type"
+                :content "text/html; charset=utf-8"}]
+        [:title "adder"]]
+      [:body content])))
+
+(defn view-input []
+  (view-layout
+    [:h2 "add two numbers"]
+    [:form {:method "post" :action "/"}
+      [:input.math {:type "text" :name "a"}] [:span.math " + "]
+      [:input.math {:type "text" :name "b"}] [:br]
+      [:input.action {:type "submit" :value "add"}]]))
+
+(defn view-output [a b sum]
+  (view-layout
+    [:h2 "two numbers added"]
+    [:p.math a " + " b " = " sum]
+    [:a.action {:href "/"} "add more numbers"]))
+
+(defn parse-input [a b]
+  [(Integer/parseInt a) (Integer/parseInt b)])
+
+(defroutes app
+   (GET "/" []
+        (view-input))
+   (POST "/" [a b]
+         (let [[a b] (parse-input a b)
+               sum (+ a b)]
+           (view-output a b sum))))
