84c625cae7787e12513c3de0d5abbaeb0f62a0a9
Scott Wyman Neagle <scott.neagle@cs.nyu.edu>
2017-12-01 14:46:23 -0600

    Revert "added mpg and byte-streams dependencies"
    This reverts commit a7bf41f408c182f65ce01fbd646b1d0eb2db82a8.

--- project.clj
+++ project.clj
@@ -8,9 +8,9 @@
                  [ring/ring-jetty-adapter "1.4.0"]
                  [environ "1.0.0"]
                  [org.clojure/java.jdbc "0.7.0"]
-                 [hiccup "1.0.5"]
                  [byte-streams "0.2.3"]
-                 [mpg "1.3.0"]]
+                 [org.postgresql/postgresql "9.4-1201-jdbc4"]
+                 [hiccup "1.0.5"]]
   :min-lein-version "2.0.0"
   :plugins [[environ/environ.lein "0.3.1"]]
   :hooks [environ.leiningen.hooks]
